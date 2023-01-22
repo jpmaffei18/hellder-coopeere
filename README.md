a45dd44790328a7c0fd56e66a44a98531ed47483
tripleAVB <venturebuildertriplea@gmail.com>
2023-01-22 15:32:06 -0300

    telas prontas pra testar a autenticação

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>FRONTEND</h1>
+
+## Este é um frontend de uma aplicação mobile que receberá o backend com Nest.js, MySQL, TypeORM, e JWT.
+
+### mobile_one - primeiro commmit
+### mobile_two - telas prontas pra autenticação, mas o app não está pronto.
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
@@ -9,4 +9,19 @@ export class Tab2Page {
 
   constructor() {}
 
+  @ViewChild('popover1') popover1: any;
+  @ViewChild('popover2') popover2: any;
+
+  isOpen1 = false;
+  isOpen2 = false;
+
+  popoverCons(e1: Event) {
+    this.popover1.event = e1;
+    this.isOpen1 = true;
+  }
+
+  popoverCooperado(e2: Event) {
+    this.popover2.event = e2;
+    this.isOpen2 = true;
+  }
 }
