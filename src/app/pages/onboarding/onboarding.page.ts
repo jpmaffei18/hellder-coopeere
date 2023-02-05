0a76ab85cefd19014a2f987182a81997476fc606
tripleAVB <venturebuildertriplea@gmail.com>
2023-02-04 22:43:22 -0300

    rotas resolvidas

--- src/app/pages/onboarding/onboarding.page.ts
+++ src/app/pages/onboarding/onboarding.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.page.html',
   styleUrls: ['./onboarding.page.scss'],
 })
-export class OnboardingPage  {
+export class OnboardingPage implements OnInit  {
  
   slideOpts = {
     autoplay: {
@@ -14,18 +15,49 @@ export class OnboardingPage  {
     loop: true
   };
 
-  constructor() {}
 
-  openExternalLinkFacebook(){
-    window.open('https://www.facebook.com', '_blank')
-  }
+  selectedOption: string;
   
-  openExternalLinkInstagram(){
-    window.open('https://www.instagram.com', '_blank')
+  constructor(private navCtrl: NavController) {
+    this.selectedOption = "1";
+
+   
   }
 
-  openExternalLinkYouTube(){
-    window.open('https://www.youtube.com', '_blank')
-  }
+
+  onOptionSelected(option: string) {
+    
+    switch (option) {
+      case "1":
+        this.navCtrl.navigateForward('/quemsomos');
+        break;
+      case "2":
+        this.navCtrl.navigateForward('/objetivos');
+        break;
+      case "3":
+        this.navCtrl.navigateForward('/equipe');
+          break;  
+    }
+
+}
+
+
+
+openExternalLinkFacebook(){
+  window.open('https://www.facebook.com', '_blank')
+}
+
+openExternalLinkInstagram(){
+  window.open('https://www.instagram.com', '_blank')
+}
+
+openExternalLinkYouTube(){
+  window.open('https://www.youtube.com', '_blank')
+}
+
+ngOnInit(): void{
+
+}
+
 
 }
