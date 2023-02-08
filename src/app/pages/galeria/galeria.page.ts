1c8657c38a53cb2a74730e37ec9267df7b5b2ea3
tripleAVB <venturebuildertriplea@gmail.com>
2023-02-08 06:54:55 -0300

    mais páginas adicionadas

--- src/app/pages/galeria/galeria.page.ts
+++ src/app/pages/galeria/galeria.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-galeria',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaPage implements OnInit {
 
-  constructor() { }
+  selectedOption: string = '';
+ 
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  goToPage(option: string) {
+    switch (option) {
+      case 'option1':
+        this.router.navigateByUrl('/option1');
+        break;
+      case 'option2':
+        this.router.navigateByUrl('/option2');
+        break;
+      case 'option3':
+        this.router.navigateByUrl('/option3');
+        break;
+      default:
+        break;
+    }
+
   }
+
+openExternalLinkFacebook(){
+  window.open('https://www.facebook.com', '_blank')
+}
+
+openExternalLinkInstagram(){
+  window.open('https://www.instagram.com', '_blank')
+}
+
+openExternalLinkYouTube(){
+  window.open('https://www.youtube.com', '_blank')
+}
+
+ngOnInit(): void{
+
+}
+
 
 }
