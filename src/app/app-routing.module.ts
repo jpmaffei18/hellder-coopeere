8582d32179ed308b3d13a3ca072eddecfe84735c
jpmaffei <jpmaffei18@gmail.com>
2023-02-21 21:40:18 -0300

    alteração_21_02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -230,12 +230,8 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/inside/home/home.module').then( m => m.HomePageModule)
-  },  {
-    path: 'exemplo',
-    loadChildren: () => import('./pages/exemplo/exemplo.module').then( m => m.ExemploPageModule)
-  }
-
-
+  },
+  
 ];
 @NgModule({
   imports: [
